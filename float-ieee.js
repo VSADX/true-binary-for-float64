6c5239ca6b60dbcb8b475997ef9fee8e37b70666
VSADX <81491645+VSADX@users.noreply.github.com>
2021-07-06 22:57:33 -0500

    Update float-ieee.js

--- float-ieee.js
+++ float-ieee.js
@@ -1,19 +1,27 @@
-export const float_ieee = {
-    bits(num = NaN, format = true) {
-        let str = new BigInt64Array(
-            new Float64Array([num]
-        ).buffer)[0]
-            .toString(2)
+export const Binary = {
+    /**
+     * Convert number to the stringified IEEE binary.
+     * @param {number} number 
+     * @param {boolean} format Adds spacing every 4 characters.
+     */
+    stringify(number, format = true) {
+        const str = new BigInt64Array(
+            new Float64Array([number]
+        ).buffer)[0].toString(2)
 
         if(!format) return str
         return str
             .padStart(64, 0)
             .replace(/\d{4}(?!$)/g, "$& ")
     },
-    num(str = "") {
+    /**
+     * Convert text containing 1s, 0s into a number as raw IEEE binary.
+     * @param {string} stringified_binary 
+     */
+    parse(stringified_binary) {
         return new Float64Array(
             new BigInt64Array(
-                [BigInt(`0b${str.replaceAll(" ","")}`)
+                [BigInt(`0b${stringified_binary.replaceAll(" ","")}`)
             ]).buffer)[0]
     }
 }
